a840e3f90a8663208034f8edccb92bb6bcb8e3f0
jawadan <35630476+jawadan@users.noreply.github.com>
2021-06-27 02:03:58 +0600

    another

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,7 +4,7 @@ import Hero from '../components/sections/Hero';
 import FeaturesTiles from '../components/sections/FeaturesTiles';
 import FeaturesSplit from '../components/sections/FeaturesSplit';
 import Testimonial from '../components/sections/Testimonial';
-import Cta from '../components/sections/Cta';
+import Newsletter from '../components/sections/Newsletter';
 
 const Home = () => {
 
@@ -14,7 +14,7 @@ const Home = () => {
       <FeaturesTiles />
       <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
       <Testimonial topDivider />
-      <Cta split />
+      <Newsletter split />
     </>
   );
 }
